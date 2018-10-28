92f40722ef5c444b15864887544b932df51788fa
Nilton Heck <nilton@conbuss.com>
2018-10-27 22:20:24 -0300

    Primeiro commit

---
+++ cnn_cifar100-fine.py
@@ -0,0 +1,101 @@
+from __future__ import print_function
+import keras
+from keras.datasets import cifar100
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras import backend as K
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+cifar100_label_list = np.array([
+    'apple', 'aquarium_fish', 'baby', 'bear', 'beaver', 'bed', 'bee', 'beetle', 
+    'bicycle', 'bottle', 'bowl', 'boy', 'bridge', 'bus', 'butterfly', 'camel', 
+    'can', 'castle', 'caterpillar', 'cattle', 'chair', 'chimpanzee', 'clock', 
+    'cloud', 'cockroach', 'couch', 'crab', 'crocodile', 'cup', 'dinosaur', 
+    'dolphin', 'elephant', 'flatfish', 'forest', 'fox', 'girl', 'hamster', 
+    'house', 'kangaroo', 'keyboard', 'lamp', 'lawn_mower', 'leopard', 'lion',
+    'lizard', 'lobster', 'man', 'maple_tree', 'motorcycle', 'mountain', 'mouse',
+    'mushroom', 'oak_tree', 'orange', 'orchid', 'otter', 'palm_tree', 'pear',
+    'pickup_truck', 'pine_tree', 'plain', 'plate', 'poppy', 'porcupine',
+    'possum', 'rabbit', 'raccoon', 'ray', 'road', 'rocket', 'rose',
+    'sea', 'seal', 'shark', 'shrew', 'skunk', 'skyscraper', 'snail', 'snake',
+    'spider', 'squirrel', 'streetcar', 'sunflower', 'sweet_pepper', 'table',
+    'tank', 'telephone', 'television', 'tiger', 'tractor', 'train', 'trout',
+    'tulip', 'turtle', 'wardrobe', 'whale', 'willow_tree', 'wolf', 'woman',
+    'worm'
+])
+
+batch_size = 128
+num_classes = 20
+epochs = 12
+
+# input image dimensions
+img_rows, img_cols = 32, 32
+
+# the data, split between train and test sets
+
+# SUPERCLASSE
+(x_train, y_train), (x_test, y_test) = cifar100.load_data(label_mode='fine')
+
+# CLASSE
+# (x_train, y_train), (x_test, y_test) = cifar100.load_data(label_mode='fine')
+
+input_shape = (img_rows, img_cols, 3)
+
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+x_train /= 255
+x_test /= 255
+print('x_train shape:', x_train.shape)
+print(x_train.shape[0], 'train samples')
+print(x_test.shape[0], 'test samples')
+
+# convert class vectors to binary class matrices
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
+
+model = Sequential()
+model.add(Conv2D(32, kernel_size=(3, 3),
+                 activation='relu',
+                 input_shape=input_shape))
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(num_classes, activation='softmax'))
+model.compile(loss=keras.losses.categorical_crossentropy,
+              optimizer=keras.optimizers.Adadelta(),
+              metrics=['accuracy'])
+
+model.fit(x_train, y_train,
+          batch_size=batch_size,
+          epochs=epochs,
+          verbose=1,
+          validation_data=(x_test, y_test))
+
+score = model.evaluate(x_test, y_test, verbose=0)
+
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
+
+y_hat = model.predict(x_test)
+
+# Plot a random sample of 10 test images, their predicted labels and ground truth
+figure = plt.figure(figsize=(20, 8))
+for i, index in enumerate(np.random.choice(x_test.shape[0], size=15, replace=False)):
+    ax = figure.add_subplot(3, 5, i + 1, xticks=[], yticks=[])
+    # Display each image
+    ax.imshow(np.squeeze(x_test[index]))
+    predict_index = np.argmax(y_hat[index])
+    true_index = np.argmax(y_test[index])
+    # Set the title for each image
+    ax.set_title("{} ({})".format(cifar100_label_list[predict_index], 
+                                  cifar100_label_list[true_index]),
+                                  color=("green" if predict_index == true_index else "red"))
+
+plt.show()
---
+++ readme.md
@@ -0,0 +1,11 @@
+Códigos para os Exercícios de Deep Learning
+====
+
+Requerimentos:
+
+Python 3.6+
+Keras
+Numpy
+matplotlib
+
+Arquivos:
